ac14a538bfedc1388ec433516f49cd82af1a2d88
Jayasima Karthikeyan <166920086+JAYASIMA007@users.noreply.github.com>
2025-01-22 14:35:49 +0530

    Update models.py

--- event_management_system_app/models.py
+++ event_management_system_app/models.py
@@ -1,5 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+STATE_CHOICES = (
+   ("AN","Andaman and Nicobar Islands"),
+   ("AP","Andhra Pradesh"),
+   ("AR","Arunachal Pradesh"),
+   ("AS","Assam"),
+   ("BR","Bihar"),
+   ("CG","Chhattisgarh"),
+   ("CH","Chandigarh"),
+   ("DN","Dadra and Nagar Haveli"),
+   ("DD","Daman and Diu"),
+   ("DL","Delhi"),
+   ("GA","Goa"),
+   ("GJ","Gujarat"),
+    ("LA","Ladakh"),
+   ("LD","Lakshadweep"),
+   ("MP","Madhya Pradesh"),
+   ("MH","Maharashtra"),
+   ("MN","Manipur"),
+   ("ML","Meghalaya"),
+   ("MZ","Mizoram"),
+   ("NL","Nagaland"),
+   ("OD","Odisha"),
+   ("PB","Punjab"),
+   ("PY","Pondicherry"),
+   ("RJ","Rajasthan"),
+   ("SK","Sikkim"),
+   ("TN","Tamil Nadu"),
+   ("TS","Telangana"),
+   ("TR","Tripura"),
+   ("UP","Uttar Pradesh"),
+   ("UK","Uttarakhand"),
+   ("WB","West Bengal")
+)
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -13,3 +47,14 @@ class Event(models.Model):
     allocated_seats = models.IntegerField(default=0)
     location = models.CharField(max_length=255, default='')
     organizer = models.CharField(max_length=100, default='')
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    locality = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
